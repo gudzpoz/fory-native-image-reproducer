feec6157189d47a0493a9ef72d2d7dff01304573
gudzpoz <gudzpoz@live.com>
2025-09-25 16:32:41 +0800

    test: reproduce fory primitive array zeroing

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,9 +2,10 @@ package org.example;
 
 import org.apache.fory.Fory;
 
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
-public class App extends AbstractMap<Integer, Integer> {
+public class App {
     private static final Fory FORY = Fory.builder()
         .registerGuavaTypes(false)
         .build();
@@ -13,22 +14,22 @@ public class App extends AbstractMap<Integer, Integer> {
         FORY.register(App.class, true);
     }
 
-    final int[] ints = new int[10];
+    final byte[] bytes = "01234567890".getBytes(StandardCharsets.UTF_8);
+    final short[] shorts = new short[]{0xF01, 0xF02, 0xF03, 0xF04, 0xF05, 0xF06, 0xF07, 0xF08};
+    final int[] ints = new int[]{0xF0F0F01, 0xF0F0F02, 0xF0F0F03, 0xF0F0F04};
+    final long[] longs = new long[]{0x0FFF_0000_FFFF_0001L, 0x0FFF_0000_FFFF_0002L};
+    final Object[] objects = {"A", "B"};
 
     public static void main(String[] args) {
         FORY.reset();
         byte[] bytes = FORY.serialize(new App());
+        System.out.println(Arrays.toString(bytes));
         FORY.reset();
         App main = (App) FORY.deserialize(bytes);
+        System.out.println(Arrays.toString(main.bytes));
+        System.out.println(Arrays.toString(main.shorts));
         System.out.println(Arrays.toString(main.ints));
-    }
-
-    @Override
-    public Set<Entry<Integer, Integer>> entrySet() {
-        HashSet<Entry<Integer, Integer>> set = new HashSet<>();
-        for (int i = 0; i < ints.length; i++) {
-            set.add(new AbstractMap.SimpleEntry<>(i, ints[i]));
-        }
-        return set;
+        System.out.println(Arrays.toString(main.longs));
+        System.out.println(Arrays.toString(main.objects));
     }
 }
