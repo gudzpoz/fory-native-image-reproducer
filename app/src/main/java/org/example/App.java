ecef3ebbb71cace7955d0a44867ee51eed85e805
gudzpoz <gudzpoz@live.com>
2025-09-26 16:52:19 +0800

    test: inheritance serialization

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,34 +2,44 @@ package org.example;
 
 import org.apache.fory.Fory;
 
-import java.nio.charset.StandardCharsets;
-import java.util.*;
-
 public class App {
     private static final Fory FORY = Fory.builder()
-        .registerGuavaTypes(false)
-        .build();
+            .withRefTracking(true)
+            .registerGuavaTypes(false)
+            .build();
 
     static {
+        FORY.register(AppInnerImpl.class, true);
         FORY.register(App.class, true);
     }
 
-    final byte[] bytes = "01234567890".getBytes(StandardCharsets.UTF_8);
-    final short[] shorts = new short[]{0xF01, 0xF02, 0xF03, 0xF04, 0xF05, 0xF06, 0xF07, 0xF08};
-    final int[] ints = new int[]{0xF0F0F01, 0xF0F0F02, 0xF0F0F03, 0xF0F0F04};
-    final long[] longs = new long[]{0x0FFF_0000_FFFF_0001L, 0x0FFF_0000_FFFF_0002L};
-    final Object[] objects = {"A", "B"};
+    AppInner inner = new AppInnerImpl("test");
 
     public static void main(String[] args) {
         FORY.reset();
-        byte[] bytes = FORY.serialize(new App());
-        System.out.println(Arrays.toString(bytes));
+        App from = new App();
+        byte[] bytes = FORY.serialize(from);
         FORY.reset();
-        App main = (App) FORY.deserialize(bytes);
-        System.out.println(Arrays.toString(main.bytes));
-        System.out.println(Arrays.toString(main.shorts));
-        System.out.println(Arrays.toString(main.ints));
-        System.out.println(Arrays.toString(main.longs));
-        System.out.println(Arrays.toString(main.objects));
+        App to = (App) FORY.deserialize(bytes);
+        assert to.inner.toString().equals("test");
+    }
+
+    private static abstract class AppInner {
+        private final String s;
+
+        AppInner(String s) {
+            this.s = s;
+        }
+
+        @Override
+        public String toString() {
+            return s;
+        }
+    }
+
+    private static class AppInnerImpl extends AppInner {
+        AppInnerImpl(String s) {
+            super(s);
+        }
     }
 }
